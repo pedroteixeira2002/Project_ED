366c5a69e60cdbd9d2d7672dd39b08c115c8955b
Miguel Ribeiro <8170538@estg.ipp.pt>
2024-01-20 17:45:53 +0000

    Dependicies OK 2.0

--- app/src/main/java/game/Game.java
+++ app/src/main/java/game/Game.java
@@ -1,9 +1,9 @@
 package game;
 
-
 public class Game {
     public static void main(String[] args) {
-        System.out.println("Teste Branch");
+        System.out.println("Hello World!");
+        System.out.println("Teste");
 
     }
 }
--- app/src/test/java/src/AppTest.java
+++ app/src/test/java/src/AppTest.java
@@ -7,7 +7,5 @@ import org.junit.jupiter.api.Test;
 
 class AppTest {
     @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 }
