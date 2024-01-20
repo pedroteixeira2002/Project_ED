03537cc8dc504542477a2783de386eeac4b9b09e
pedroteixeira2002 <8200489@estg.ipp.pt>
2024-01-20 16:58:40 +0000

    Revert "Merge pull request #1 from pedroteixeira2002/teixeira"
    This reverts commit 4ed355e7c286a2710b304b268dced74e2589a73e, reversing
    changes made to 932404d58c9b62f70f4c1927864cbc3ef1056a2e.

---
+++ app/src/main/java/game/Algorithm.java
@@ -0,0 +1,16 @@
+package game;
+
+public abstract class Algorithm {
+
+
+    private void move() {
+
+    }
+    public static void attack() {
+
+    }
+    public static void defend() {
+
+    }
+    public static
+}
