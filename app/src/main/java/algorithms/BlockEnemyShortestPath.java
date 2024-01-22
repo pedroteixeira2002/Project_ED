8237a509693951dfa2d63935c191a106895663ee
pedroteixeira2002 <8200489@estg.ipp.pt>
2024-01-22 00:27:49 +0000

    update ShortestEnemy

--- app/src/main/java/algorithms/BlockEnemyShortestPath.java
+++ app/src/main/java/algorithms/BlockEnemyShortestPath.java
@@ -3,6 +3,9 @@ package algorithms;
 import game.Game;
 import game.Location;
 import interfaces.IAlgorithm;
+import structures.NetworkEnhance;
+
+import java.util.Iterator;
 
 public class BlockEnemyShortestPath extends Algorithm implements IAlgorithm {
     public BlockEnemyShortestPath(Game game) {
@@ -11,6 +14,18 @@ public class BlockEnemyShortestPath extends Algorithm implements IAlgorithm {
 
     @Override
     public Location move(Game game) {
-        return null;
+        NetworkEnhance<Location> tempMap = super.getMap();
+        Iterator<Location> list = tempMap.iteratorShortestPath
+                (super.getOpponentFlag(), super.getMyLocation());
+
+        botInTheWay(tempMap, list);
+
+        double shortestPathWeight = tempMap.shortestPathWeight
+                (super.getOpponentFlag(), super.getMyLocation());
+        System.out.println("Actual Enemy Shortest Path Weight : " + shortestPathWeight);
+
+        setMyLocation(list.next());
+
+        return getMyLocation();
     }
 }
